80d98470e98360a717fa8f104fa6f4d482db91b7
Stiffly <adambylehn@hotmail.com>
2015-09-09 15:49:53 +0200

    Skeleton for Sound system

---
+++ include/Core/Sound.h
@@ -0,0 +1,19 @@
+//
+// Created by Adam on 2015-09-09.
+//
+
+#ifndef DAYDREAM_SOUND_H
+#define DAYDREAM_SOUND_H
+
+namespace dd
+{
+class Sound
+{
+public:
+    void HelloWorld();
+private:
+
+};
+}
+
+#endif //DAYDREAM_SOUND_H
---
+++ src/game/Core/Sound.cpp
@@ -0,0 +1,9 @@
+//
+// Created by Adam on 2015-09-09.
+//
+#include "Core/Sound.h"
+
+void dd::Sound::HelloWorld()
+{
+    std::cout << "Hej världen!" << std::endl;
+}
