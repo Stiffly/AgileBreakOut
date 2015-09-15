5e39ac3629fd0a7213093810dd8b0146b29a6847
tleety <dahltobbe@hotmail.com>
2015-09-15 18:20:26 +0200

    Added a low poly brick model and changed their ingame scale

--- src/game/Game/LevelSystem.cpp
+++ src/game/Game/LevelSystem.cpp
@@ -55,7 +55,7 @@ void dd::Systems::LevelSystem::CreateBrick(int row, int line, glm::vec2 spacesBe
     model->ModelFile = "Models/Test/Brick/Brick.obj";
     float x = spaceToEdge + line * spacesBetweenBricks.x;
     float y = spaceToEdge + row * spacesBetweenBricks.y;
-    transform->Scale = glm::vec3(1, 0.4, 0.2);
+    transform->Scale = glm::vec3(1.6, 0.2, 0.5);
     transform->Position = glm::vec3(x - 7, y + 1, -10.f);
     //sprite->Color = glm::vec4(1.f, 1.f, 1.f, 1.f);
     m_World->CommitEntity(brick);
