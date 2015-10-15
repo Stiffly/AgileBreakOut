f33595539617f017c575f9c674f7c502fed6cae0
sippeangelo <holmberg.simon@gmail.com>
2015-10-15 18:39:47 +0200

    Animation system now listens to pause events

--- include/Rendering/AnimationSystem.h
+++ include/Rendering/AnimationSystem.h
@@ -4,6 +4,7 @@
 #include "Core/System.h"
 #include "Core/World.h"
 #include "Rendering/CAnimation.h"
+#include "Game/EPause.h"
 
 namespace dd
 {
@@ -17,7 +18,14 @@ public:
 		: System(world, eventBroker) { }
 
 	void RegisterComponents(ComponentFactory* cf) override;
+	void Initialize() override;
 	void Update(double dt) override;
+	
+private:
+	bool m_Paused = false;
+
+	EventRelay<AnimationSystem, Events::Pause> m_EPause;
+	bool OnPause(const Events::Pause& e);
 };
 
 }
--- src/game/Rendering/AnimationSystem.cpp
+++ src/game/Rendering/AnimationSystem.cpp
@@ -6,8 +6,17 @@ void dd::Systems::AnimationSystem::RegisterComponents(ComponentFactory* cf)
 	cf->Register<Components::Animation>();
 }
 
+void dd::Systems::AnimationSystem::Initialize()
+{
+	EVENT_SUBSCRIBE_MEMBER(m_EPause, &AnimationSystem::OnPause);
+}
+
 void dd::Systems::AnimationSystem::Update(double dt)
 {
+	if (m_Paused) {
+		return;
+	}
+
 	auto animations = m_World->GetComponentsOfType<Components::Animation>();
 	if (animations == nullptr) {
 		return;
@@ -17,4 +26,10 @@ void dd::Systems::AnimationSystem::Update(double dt)
 		auto animation = static_cast<Components::Animation*>(component.get());
 		animation->Time += animation->Speed * dt;
 	}
+}
+
+bool dd::Systems::AnimationSystem::OnPause(const Events::Pause& e)
+{
+	m_Paused = !m_Paused;
+	return true;
 }
